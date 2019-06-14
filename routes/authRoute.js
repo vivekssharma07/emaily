4b6b8db176d3610a8bdef63cefa0920f7218df2b
Vivek <vivekssharma07@gmail.com>
2019-06-14 17:33:36 +0530

    Build

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     app.get('/auth/google',passport.authenticate('google',{ scope:['profile','email']}));
     
     app.get('/auth/google/callback',passport.authenticate('google'),(req,res) =>{
-        res.redirect('/surveys')
+        res.redirect('/')
     })
     
     app.get('/api/logout',(req,res) => {
