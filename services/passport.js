2e20a211ef5e8734b947d2bb23b19a055ef4cbb9
Vivek <vivekssharma07@gmail.com>
2019-06-17 14:50:50 +0530

    GIt HUB

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,7 @@ passport.use(new GoogleStratergy(
     {
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
-    callbackURL : 'http://localhost:5000/auth/google/callback',
+    callbackURL : '/auth/google/callback',
     proxy :true
     },
     async (accessToken,refreshToken,profile,done) => {
